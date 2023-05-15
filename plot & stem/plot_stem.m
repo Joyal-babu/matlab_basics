910308b3fea4fc5af2634bec15b898bf91bbaf33
JOYAL BABU <joyalbabujp21@gmail.com>
2023-05-15 12:13:45 +0530

    Update plot_stem.m

--- plot & stem/plot_stem.m
+++ plot & stem/plot_stem.m
@@ -1,4 +1,4 @@
-x=[1 2 3 4];
+x=[1 2 3 4];           %while dealing with the array data is MATLAB index starts with 1
 subplot(2,1,1);
 plot(x,'ro--');        % r-red ,  o-o marker , - solid line , -- dashed line
 title('Continuous');
