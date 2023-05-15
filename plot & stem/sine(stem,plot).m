8a086fce536546d872ec08579a285ff849f70408
JOYAL BABU <joyalbabujp21@gmail.com>
2023-05-15 12:19:00 +0530

    Create sine(stem,plot).m

---
+++ plot & stem/sine(stem,plot).m
@@ -0,0 +1,8 @@
+x=linspace(0,2*pi,25); # 0 to 2pi is divided in 25 equidistant points
+y=sin(x);
+subplot(2,1,1);
+plot(x,y,'r');
+title('Continuous');
+subplot(2,1,2);
+stem(x,y);
+title('Discrete');
